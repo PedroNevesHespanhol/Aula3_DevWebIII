dc50016ab6c25f7208110d141feb7e41f7108406
PedroNevesHespanhol <88465619+PedroNevesHespanhol@users.noreply.github.com>
2023-04-02 17:18:37 -0300

    Aula3_DevWebII

---
+++ pokedex/src/components/Skeletons/index.jsx
@@ -0,0 +1,11 @@
+import { Skeleton } from "@mui/material";
+import { Container } from "@mui/system";
+import React from "react";
+
+export const Skeletons = () => {
+  return (
+    <Container maxWidth={false}>
+      <Skeleton variant="rounded" width="15%" height={250} sx={{ marginBottom: "1em" }} />
+    </Container>
+  );
+};
